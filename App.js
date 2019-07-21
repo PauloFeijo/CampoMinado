a9198f5321fc5b17bbd2565fdb97fd16eeda3ac1
PauloFeijo <paulorikrdo.89@gmail.com>
2019-07-21 11:09:04 -0300

    Criação do componente Field

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { StyleSheet, View, Text } from 'react-native'
 import params from './src/params'
+import Field from './src/components/Field'
 
 export default class App extends Component {
   render() {
@@ -10,6 +11,7 @@ export default class App extends Component {
         <Text>Tamanho da grade:
           {params.getRowsAmount()}x{params.getColumnsAmount()}
         </Text>
+        <Field/>
       </View>
     )
   }
