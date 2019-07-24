e59149a4262370eb37cf6ff137fe42daedd5dcfb
PauloFeijo <paulorikrdo.89@gmail.com>
2019-07-23 21:53:02 -0300

    Flag Bigger

--- src/components/Flag.js
+++ src/components/Flag.js
@@ -4,8 +4,10 @@ import { View, StyleSheet } from 'react-native'
 export default props => {
     return (
         <View style={styles.container}>
-            <View style={styles.flagpole}/>
-            <View style={styles.flag}/>
+            <View style={[styles.flagpole, props.bigger ? styles.flagpoleBigger : null]}/>
+            <View style={[styles.flag, props.bigger ? styles.flagBigger : null]}/>
+            <View style={[styles.base1, props.bigger ? styles.base1Bigger : null]}/>
+            <View style={[styles.base2, props.bigger ? styles.base2Bigger : null]}/>
         </View>
     )
 }
@@ -36,12 +38,34 @@ const styles = StyleSheet.create({
         marginLeft: 7,
         marginTop: 10       
     },
-    base1: {
+    base2: {
         position: 'absolute',
         height: 2,
         width: 10,
         backgroundColor: '#222',
         marginLeft: 5,
         marginTop: 12       
-    }    
+    },
+    flagpoleBigger: {
+        height: 28,
+        width: 4,
+        marginLeft: 16
+    },
+    flagBigger: {
+        height: 10,
+        width: 14,
+        marginLeft: 3
+    },
+    base1Bigger: {
+        height: 4,
+        width: 12,
+        marginLeft: 12,
+        marginTop: 20       
+    },
+    base2Bigger: {
+        height: 4,
+        width: 20,
+        marginLeft: 8,
+        marginTop: 24       
+    }
 })
